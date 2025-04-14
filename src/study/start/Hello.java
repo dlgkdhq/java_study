bacf871df1cac98e724eea1240cec8e3c2a02874
dlgkdhq <dlgkdms920@naver.com>
2025-04-14 11:35:37 +0900

    자바 시작, gitignore 추가

---
+++ src/study/start/Hello.java
@@ -0,0 +1,17 @@
+package study.start;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World!!");
+		
+		
+		// 주석
+		// 실행(Run) : Ctrl + F11
+		
+		
+	}
+
+}
