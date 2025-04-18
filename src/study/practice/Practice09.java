cdfc6c6fb19cc296eb6fc72ec0ef263e9954f8c8
dlgkdhq <dlgkdms920@naver.com>
2025-04-18 12:39:04 +0900

    연습9 반복문

---
+++ src/study/practice/Practice09.java
@@ -0,0 +1,119 @@
+package study.practice;
+
+public class Practice09 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+//		1)
+		for(int i=0; i<=10; i++) {
+			System.out.print(i + " ");
+		}
+		System.out.println();
+		
+//		i=0;
+//		while(i<=10) {
+//			System.out.println(i);
+//			i++;
+//		}
+		
+//		2)
+		for(int i=1; i<10; i++) {
+			System.out.print(i + " ");
+		}
+		System.out.println();
+		
+//		i=1;
+//		while(i<10) {
+//			System.out.println(i);
+//			i++;
+//		}
+		
+//		3)
+		for(int i=10; i<=100; i+=10) {
+			System.out.print(i + " ");
+		}
+		System.out.println();
+		
+//		i=10;
+//		while(i<=100) {
+//			System.out.println(i);
+//			i+=10;
+//		}
+		
+//		4)
+		for(int i=25; i<=100; i+=25) {
+			System.out.print(i + " ");
+		}
+		System.out.println();
+		
+//		i=25;
+//		while(i<=100) {
+//			System.out.println(i);
+//			i+=25;
+//		}
+		
+//		5)
+		int i=50;
+		while(i<=55) {
+			System.out.print(i + " ");
+			i++;
+		}
+		System.out.println();
+		
+//		for(i=50; i<=55; i++) {
+//			System.out.println(i);
+//		}
+		
+//		6)
+		i=10;
+		while(i>0) {
+			System.out.print(i + " ");
+			i--;
+		}
+		System.out.println();
+		
+//		for(i=10; i>=1; i--) {
+//			System.out.println(i);
+//		}
+		
+//		7)
+		i=30;
+		while(i>=0) {
+			System.out.print(i + " ");
+			i -= 5;
+		}
+		System.out.println();
+		
+//		for(i=30; i>=0; i-=5) {
+//			System.out.println(i);
+//		}
+		
+//		8)
+		i=30;
+		while(i>=5) {
+			System.out.print(i + " ");
+			i -= 5;
+		}
+		System.out.println();
+		
+//		for(i=30; i>=5; i-=5) {
+//			System.out.println(i);
+//		}
+		
+//		9)
+		i=25;
+		while(i>=0) {
+			System.out.print(i + " ");
+			i -= 5;
+		}
+		System.out.println();
+		
+//		for(i=25; i>=0; i-=5) {
+//			System.out.println(i);
+//		}
+	}
+
+	
+}
