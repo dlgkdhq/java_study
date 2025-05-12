33e6ae8234192b4a1c6d916467163b34a237e973
dlgkdhq <dlgkdms920@naver.com>
2025-05-12 14:44:53 +0900

    연습42 클래스 구조

---
+++ src/study/practice/practice42/Room.java
@@ -0,0 +1,16 @@
+package study.practice.practice42;
+
+public class Room {
+	String type;
+	
+}
+
+class LectureRoom extends Room{
+	//강의실
+}
+class RestRoom extends Room{
+	//휴게실
+}
+class BusinessRoom extends Room{
+	//사무실
+}
